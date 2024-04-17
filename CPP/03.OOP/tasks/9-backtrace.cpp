ecb2d0803ffebf689f2d06972cf6f26e99c7eecb
hlarda <halareda63@gmail.com>
2024-04-17 09:42:36 +0200

    implemented backtrace stack with vector

--- CPP/03.OOP/tasks/9-backtrace.cpp
+++ CPP/03.OOP/tasks/9-backtrace.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-#define Enter       cout << "Entered:     "<<__PRETTY_FUNCTION__ << endl;
-#define EXIT         cout << "Terminated:    "<<__PRETTY_FUNCTION__ << endl;
+/*he's asked for class but i think there is no need*/
+vector<string> backtrace;
+
+#define Enter       cout << "Entered:     "<<__PRETTY_FUNCTION__ << endl;backtrace.push_back(__PRETTY_FUNCTION__);
+#define EXIT        cout << "Terminated:    "<<__PRETTY_FUNCTION__ << endl;backtrace.push_back(__PRETTY_FUNCTION__);
+#define PRINT_BT    cout << "Backtrace: " << endl;for(string s : backtrace){ cout << s << endl; }backtrace.clear();
 /*
 *Entered:     int main(int, const char**)
 *Entered:     void fun()
@@ -26,5 +31,6 @@ int main(int argc, char const *argv[])
     Enter;
     fun();
     EXIT;
+    PRINT_BT;
     return 0;
 }
